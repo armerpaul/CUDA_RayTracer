ec2b5448c4a318124a056337acb0e7b1aa856aec
mcrussell <macrusse@calpoly.edu>
2013-02-03 20:29:49 -0800

    Cleaned up code

--- Makefile
+++ Makefile
@@ -3,9 +3,9 @@ GFLAGS = -g
 NVCC = nvcc
 NVFLAGS = -g -G -gencode arch=compute_20,code=sm_20
 LIBS = -lcudart
-all: cpu gpu
-gpu: Image.cpp main.cu VanExLib.h types.h
-	$(NVCC) $(NVFLAGS) -o gpu Image.cpp main.cu $(LIBS)
+all: gpu
+gpu: Image.cpp cudaRayTrace.cu cudaRayTrace.h types.h
+	$(NVCC) $(NVFLAGS) -o gpu Image.cpp cudaRayTrace.cu $(LIBS)
 cpu: Image.cpp main.cpp VanExLib.h types.h
 	g++ -o cpu -g Image.cpp main.cpp 
 clean:
---
+++ cudaRayTrace.h
@@ -0,0 +1,23 @@
+/*
+ * CPE 570 && CPE 458 Duet
+ * Ray Tracer Header
+ * Professor Christopher Lupo and Professor Zoe" Wood
+ * Paul Armer(parmer), Bryan Ching(bcching), Matt Crussell(macrusse)
+ */
+
+#ifndef VANEXLIB_H
+#define VANEXLIB_H
+
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 768
+#define SCREEN_DISTANCE -1
+#define NUM_WALLS 1
+#define NUM_SPHERES 100
+
+#define AMBIENT .2
+#define DIFFUSE .6
+#define SPECULAR .4
+
+#define FOV 45 * 3.141592653589793 / 180
+
+#endif
